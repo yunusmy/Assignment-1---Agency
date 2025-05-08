93e612cc38faa2786ff600f0a44dc51248dd0b9c
yunusmy <yunusmy>
2025-05-08 21:08:18 +0600

    added is a svg icon

---
+++ src/components/icons/ShoppingCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ShoppingCard = () => {
+  return (
+    <>
+      <svg
+        className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+      </svg>
+    </>
+  );
+};
+
+export default ShoppingCard;
---
+++ src/components/icons/UsersIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UsersIcon = () => {
+  return (
+    <>
+      <svg
+        className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+      </svg>
+    </>
+  );
+};
+
+export default UsersIcon;
